6ad05fe6182478abeac63c66c35960c6bee4c5f5
MuhammadInam <nudmaufon@gmail.com>
2024-03-20 13:28:44 -0400

    this calculator is simple but insahallah more feature add

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+
+const answere = await inquirer.prompt([
+    {
+        message: "Enter the first number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter the second number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select the operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+    },
+]);
+
+//conditional statement
+if (answere.operator === "Addition") {
+    console.log(answere.firstNumber + answere.secondNumber)
+}
+else if (answere.operator === "Subtraction") {
+    console.log(answere.firstNumber - answere.secondNumber)
+}
+else if (answere.operator === "division") {
+    console.log(answere.firstNumber / answere.secondNumber)
+}
+else if (answere.operator === "multiplication") {
+    console.log(answere.firstNumber * answere.secondNumber)
+}
